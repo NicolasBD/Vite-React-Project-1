b91f4ae2aa8b601565f285ae25ab11530b40f105
NicolasBD <nicofilnicolas9@gmail.com>
2025-04-06 15:37:27 -0500

    Add: Set up Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-project-1/"
+  base: "/Vite-React-Project-1/"
 })
